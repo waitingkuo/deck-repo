ebd5cbb900b8a9e1691e5b9826b365eeffd7f47e
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2016-09-04 22:03:16 +0800

    uncomment

--- etcd/upload.sh
+++ etcd/upload.sh
@@ -9,11 +9,11 @@ TAR_FILENAME=etcd-$VERSION-$PLATFORM-amd64.tar.gz
 DIR=etcd-$VERSION-$PLATFORM-amd64
 
 echo "Downloading $URL"
-#curl -L $URL -o /tmp/
+curl -L $URL -o /tmp/
 
 
 cd /tmp
-#tar zxvf TAR_FILENAME
+tar zxvf TAR_FILENAME
 cd /tmp/$DIR
 mc cp etcd minio/deck/etcd/etcd
 mc cp etcdctl minio/deck/etcd/etcdctl
