050dc37ccb3bd7ee301007f114ca8d3ca98cce8b
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2016-09-04 21:57:31 +0800

    add etcd

---
+++ etcd/upload.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PLATFORM=linux
+VERSION=v3.0.7
+
+
+URL=https://github.com/coreos/etcd/releases/download/$VERSION/etcd-$VERSION-$PLATFORM-amd64.tar.gz
+TAR_FILENAME=etcd-$VERSION-$PLATFORM-amd64.tar.gz
+DIR=etcd-$VERSION-$PLATFORM-amd64
+
+echo "Downloading $URL"
+#curl -L $URL -o /tmp/
+
+
+cd /tmp
+#tar zxvf TAR_FILENAME
+cd /tmp/$DIR
+mc cp etcd minio/deck/etcd/etcd
+mc cp etcdctl minio/deck/etcd/etcdctl
+
