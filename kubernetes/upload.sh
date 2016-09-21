cab976606f7b86345de449cffae91cd5c26cd17c
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2016-09-21 10:16:47 +0000

    add 1.4.0 beta.9

--- kubernetes/upload.sh
+++ kubernetes/upload.sh
@@ -7,6 +7,7 @@ VERSION=v1.4.0-beta.5
 VERSION=v1.4.0-beta.6
 VERSION=v1.4.0-beta.7
 VERSION=v1.4.0-beta.8
+VERSION=v1.4.0-beta.9
 URL=https://github.com/kubernetes/kubernetes/releases/download/$VERSION/kubernetes.tar.gz
 
 DIR=kubernetes
